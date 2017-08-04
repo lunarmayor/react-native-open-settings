b9171e0274cc8fd478ad3181ffa7d8ed1a79dd74
alphasp <gmerudotcom@gmail.com>
2017-08-04 21:32:33 +0800

    Remove createJSModules override - RN 0.47 compatibility

--- android/src/main/java/com/opensettings/OpenSettingsPackage.java
+++ android/src/main/java/com/opensettings/OpenSettingsPackage.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class OpenSettingsPackage implements ReactPackage {
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
